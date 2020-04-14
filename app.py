dd0218e4413282cc29187bcbda9a23bcb0ac8b2d
Dion <dion.bedford@gmail.com>
2020-04-14 14:10:38 +1200

    Initial Import

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def hello_world():
+    return render_template('index.html', title='Home' )
+
+@app.route('/inspiration')
+def insp_page():
+    return render_template('inspiration.html', title='Inspiration')
+
+@app.route('/loadData')
+def data_loader():
+    return "Success"
+
+if __name__ == '__main__':
+    app.run()
