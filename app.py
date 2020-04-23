7ffea57aebbe06dd40537dbf2cc9bb44a3ace576
Dion <dion.bedford@gmail.com>
2020-04-23 12:30:12 +1200

    Cleaned up code. Added connection for local DB

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 from mongoengine import *
 
-connect(
-    'testDb',
-    username='heroku_c1wldm92',
-    password='eomrqnfplp7782hecehq4ahchh',
-    host='mongodb://heroku_c1wldm92:eomrqnfplp7782hecehq4ahchh@ds113626.mlab.com:13626/heroku_c1wldm92?retryWrites=false',
-    port=13626
-)
+#################################################################
+#  Connect to Mongo. URI is for Prod. Single line connect is    #
+#  for local dev environment.                                   #
+#################################################################
 
+# connect(
+#     username='heroku_c1wldm92',
+#     password='eomrqnfplp7782hecehq4ahchh',
+#     host='mongodb://heroku_c1wldm92:eomrqnfplp7782hecehq4ahchh@ds113626.mlab.com:13626/heroku_c1wldm92?retryWrites=false',
+#     port=13626
+# )
+
+connect('devEnv')
+
+#####################################
+#   Create Tables in the database   #
+#####################################
 class User(Document):
     email = StringField()
     first_name = StringField()
@@ -18,16 +27,25 @@ class Country(Document):
     name = StringField()
     population = IntField()
 
+
+#########################
+#   Add Data to Mongo   #
+#########################
 nz = Country(name='New Zealand', population=2)
 nz.save()
 
 adon = User(first_name='Adon', last_name='Moskal')
 adon.save()
 
-app = Flask(__name__)
-
-# set the project root directory as the static folder, you can set others.
+############################################################################
+# set the project root directory as the static folder, you can set others. #
+# old = " app = Flask(__name__) "                                          #
+############################################################################
 app = Flask(__name__, static_url_path='')
+
+#################
+#  App Routes   #
+#################
 
 @app.route('/')
 @app.route('/index')
@@ -43,6 +61,9 @@ def insp_page():
 def data_loader():
     return "Success"
 
+#############################################################
+# User GET API`S Mainly used for testing. May remove later  #
+#############################################################
 @app.route('/users', methods=['GET'])
 @app.route('/users/<user_id>', methods=['GET'])
 def getUsers(user_id=None):
@@ -53,6 +74,10 @@ def getUsers(user_id=None):
         users = User.objects.get(id=user_id)
     return users.to_json()
 
+
+#############################################################
+#   Country GET API`S for both single and multi record.     #
+#############################################################
 @app.route('/countries', methods=['GET'])
 @app.route('/countries/<count_id>', methods=['GET'])
 def getCountries(count_id=None):
@@ -63,8 +88,8 @@ def getCountries(count_id=None):
         countries = Country.objects.get(id=count_id)
     return countries.to_json()
 
+#################
+#   Run App.    #
+#################
 if __name__ == '__main__':
     app.run()
-
-
-#host='118.148.27.38', port=80
