6b2bb41ea8d4db7a47aa1578acfbd6487a74a6bd
Dion <dion.bedford@gmail.com>
2020-05-03 14:57:40 +1200

    Made face learning d3

--- static/scripts.js
+++ static/scripts.js
@@ -88,7 +88,45 @@ function deleteCountry() {
 ###################*/
 
 function d3Svg(){
+
     const svg = d3.select('svg');
-    svg.style('background-color','orange');
-    console.log("yolo")
+    svg.style('background-color','purple');
+
+    const width = +(svg.attr('width'));
+    const height = +(svg.attr('height'));
+
+    const circle = svg.append('circle')
+        .attr('r',230)
+        .attr('cy', height / 2)
+        .attr('cx', width / 2);
+
+    const leftEye = svg.append('circle')
+        .attr('r',30)
+        .attr('cy', height / 2 - 100)
+        .attr('cx', width / 2 - 60 )
+        .attr('fill', 'orange');
+
+    const rightEye = svg.append('circle')
+        .attr('r',30)
+        .attr('cy', height / 2 - 100)
+        .attr('cx', width / 2 + 60)
+        .attr('fill', 'orange');
+
+        const nose = svg.append('circle')
+        .attr('r',20)
+        .attr('cy', height / 2)
+        .attr('cx', width / 2)
+        .attr('fill', 'orange');
+
+    const  g = svg.append('g')
+        .attr('transform', 'translate(480, 250)');
+
+    const Mouth = g.append('path')
+    .attr('d', d3.arc()({
+        innerRadius:140,
+        outerRadius: 170,
+        startAngle: Math.PI /2,
+        endAngle: Math.PI * 3 / 2
+    }))
+    .attr('fill', 'orange');
 };
